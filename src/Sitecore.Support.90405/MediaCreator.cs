54d1ff7fa78166ed42574883ea75153210d93b2d
Zadli <zadlizah@gmail.com>
2019-03-15 10:03:26 +0800

    Revert "Commit Second Time"
    This reverts commit a8f464331de65506e8418bae171de8594f7a45c3.

---
+++ src/Sitecore.Support.90405/MediaCreator.cs
@@ -0,0 +1,161 @@
+﻿using Sitecore.Configuration;
+using Sitecore.Data;
+using Sitecore.Data.Fields;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.Globalization;
+using Sitecore.IO;
+using Sitecore.Resources.Media;
+using Sitecore.SecurityModel;
+using System;
+
+namespace Sitecore.Support.Resources.Media
+{
+    public class MediaCreator : Sitecore.Resources.Media.MediaCreator
+    {
+        protected override Item CreateItem(string itemPath, string filePath, MediaCreatorOptions options)
+        {
+            Assert.ArgumentNotNullOrEmpty(itemPath, "itemPath");
+            Assert.ArgumentNotNullOrEmpty(filePath, "filePath");
+            Assert.ArgumentNotNull(options, "options");
+            Item item2;
+            using (new SecurityDisabler())
+            {
+                Database database = this.GetDatabase(options);
+                Item item = options.OverwriteExisting ? database.GetItem(itemPath, options.Language) : null;
+                Item parentFolder = this.GetParentFolder(itemPath, options);
+                string itemName = this.GetItemName(itemPath);
+                if (item != null && !item.HasChildren && item.TemplateID != TemplateIDs.MediaFolder)
+                {
+                    item2 = item;
+                    item2.Database.WorkflowProvider.GetWorkflow(item2);
+                    item2 = item2.Database.GetItem(item2.ID, item2.Language, Data.Version.Latest);
+                    item2.Versions.RemoveAll(true);
+                    item2 = item2.Versions.AddVersion();
+                    Assert.IsNotNull(item2, "item");
+                    item2.Editing.BeginEdit();
+                    foreach (Field field in item2.Fields)
+                    {
+                        #region Support.527357
+
+                        // Zadli 14th March 2019. Made changes to fix Ticket #527357
+                        // Basically we do not reset the workflow state and workflow when we upload and overwrite the files.
+
+                        if (field.Name != "__Workflow state" && field.Name != "__Workflow")
+                        {
+                            field.Reset();
+                        }
+
+                        #endregion
+                    }
+                    item2.Editing.EndEdit();
+                    item2.Editing.BeginEdit();
+                    item2.Name = itemName;
+                    item2.TemplateID = this.GetItemTemplate(filePath, options).ID;
+                    item2.Editing.EndEdit();
+                }
+                else
+                {
+                    item2 = parentFolder.Add(itemName, this.GetItemTemplate(filePath, options));
+                }
+                Assert.IsNotNull(item2, typeof(Item), "Could not create media item: '{0}'.", new object[]
+                {
+                    itemPath
+                });
+                Language[] array;
+                if (!options.Versioned)
+                {
+                    array = item2.Database.Languages;
+                }
+                else
+                {
+                    array = new Language[]
+                    {
+                        item2.Language
+                    };
+                }
+                Language[] array2 = array;
+                string extension = FileUtil.GetExtension(filePath);
+                Language[] array3 = array2;
+                for (int i = 0; i < array3.Length; i++)
+                {
+                    Language language = array3[i];
+                    MediaItem mediaItem = item2.Database.GetItem(item2.ID, language, Data.Version.Latest);
+                    if (mediaItem != null)
+                    {
+                        using (new EditContext(mediaItem, SecurityCheck.Disable))
+                        {
+                            mediaItem.Extension = StringUtil.GetString(new string[]
+                            {
+                                mediaItem.Extension,
+                                extension
+                            });
+                            mediaItem.FilePath = this.GetFullFilePath(item2.ID, filePath, itemPath, options);
+                            mediaItem.Alt = StringUtil.GetString(new string[]
+                            {
+                                mediaItem.Alt,
+                                options.AlternateText
+                            });
+                            mediaItem.InnerItem.Statistics.UpdateRevision();
+                        }
+                    }
+                }
+            }
+            item2.Reload();
+            return item2;
+        }
+
+        private TemplateItem GetItemTemplate(string filePath, MediaCreatorOptions options)
+        {
+            Assert.ArgumentNotNull(filePath, "filePath");
+            Assert.ArgumentNotNull(options, "options");
+            string extension = FileUtil.GetExtension(filePath);
+            string template = MediaManager.Config.GetTemplate(extension, options.Versioned);
+            Assert.IsNotNullOrEmpty(template, "Could not find template for extension '{0}' (versioned: {1}).", new object[]
+            {
+                extension,
+                options.Versioned
+            });
+            TemplateItem templateItem = this.GetDatabase(options).Templates[template];
+            Assert.IsNotNull(templateItem, typeof(TemplateItem), "Could not find item template for media. Template: '{0}'", new object[]
+            {
+                template
+            });
+            return templateItem;
+        }
+
+        private Database GetDatabase(MediaCreatorOptions options)
+        {
+            Assert.ArgumentNotNull(options, "options");
+            Database arg_23_0;
+            if ((arg_23_0 = options.Database) == null)
+            {
+                arg_23_0 = (Context.ContentDatabase ?? Context.Database);
+            }
+            return Assert.ResultNotNull<Database>(arg_23_0);
+        }
+
+        private Item GetParentFolder(string itemPath, MediaCreatorOptions options)
+        {
+            Assert.ArgumentNotNull(itemPath, "itemPath");
+            Assert.ArgumentNotNull(options, "options");
+            string[] array = StringUtil.Divide(itemPath, '/', true);
+            return this.CreateFolder((array.Length > 1) ? array[0] : "/sitecore/media library", options);
+        }
+
+        private string GetItemName(string itemPath)
+        {
+            Assert.ArgumentNotNull(itemPath, "itemPath");
+            string lastPart = StringUtil.GetLastPart(itemPath, '/', string.Empty);
+            if (!string.IsNullOrEmpty(lastPart))
+            {
+                return lastPart;
+            }
+            if (Settings.Media.IncludeExtensionsInItemNames)
+            {
+                throw new InvalidOperationException("Invalid item path for media item: " + itemPath);
+            }
+            return "unnamed";
+        }
+    }
+}
---
+++ src/Sitecore.Support.90405/Save.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.IO;
+using Sitecore.Support.Resources.Media;
+using Sitecore.SecurityModel;
+using Sitecore.Web;
+using Sitecore.Zip;
+using Sitecore.Pipelines.Upload;
+
+
+namespace Sitecore.Support.Pipelines.Upload
+{
+    /// <summary>
+	/// Saves the uploaded files.
+	/// </summary>
+	public class Save : UploadProcessor
+    {
+        /// <summary>
+        /// Runs the processor.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        /// <exception cref="T:System.Exception"><c>Exception</c>.</exception>
+        public void Process(UploadArgs args)
+        {
+            Assert.ArgumentNotNull(args, "args");
+            for (int i = 0; i < args.Files.Count; i++)
+            {
+                HttpPostedFile httpPostedFile = args.Files[i];
+                if (!string.IsNullOrEmpty(httpPostedFile.FileName))
+                {
+                    try
+                    {
+                        bool flag = UploadProcessor.IsUnpack(args, httpPostedFile);
+                        if (args.FileOnly)
+                        {
+                            if (flag)
+                            {
+                                Save.UnpackToFile(args, httpPostedFile);
+                            }
+                            else
+                            {
+                                string filename = this.UploadToFile(args, httpPostedFile);
+                                if (i == 0)
+                                {
+                                    #region Support.527357
+                                    
+                                    // Zadli 14th March 2019. Made changes to fix Ticket #527357
+                                    //args.Properties["filename"] = FileHandle.GetFileHandle(filename);
+                                    args.Properties[filename] = FileHandle.GetFileHandle(filename);
+                                    
+                                    #endregion
+                                }
+                            }
+                        }
+                        else
+                        {
+                            MediaUploader mediaUploader = new MediaUploader
+                            {
+                                File = httpPostedFile,
+                                Unpack = flag,
+                                Folder = args.Folder,
+                                Versioned = args.Versioned,
+                                Language = args.Language,
+                                AlternateText = args.GetFileParameter(httpPostedFile.FileName, "alt"),
+                                Overwrite = args.Overwrite,
+                                FileBased = (args.Destination == UploadDestination.File)
+                            };
+                            List<MediaUploadResult> list;
+                            using (new SecurityDisabler())
+                            {
+                                list = mediaUploader.Upload();
+                            }
+                            Log.Audit(this, "Upload: {0}", new string[]
+                            {
+                                httpPostedFile.FileName
+                            });
+                            foreach (MediaUploadResult current in list)
+                            {
+                                this.ProcessItem(args, current.Item, current.Path);
+                            }
+                        }
+                    }
+                    catch (Exception exception)
+                    {
+                        Log.Error("Could not save posted file: " + httpPostedFile.FileName, exception, this);
+                        throw;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Processes the item.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        /// <param name="mediaItem">The media item.</param>
+        /// <param name="path">The path.</param>
+        private void ProcessItem(UploadArgs args, MediaItem mediaItem, string path)
+        {
+            Assert.ArgumentNotNull(args, "args");
+            Assert.ArgumentNotNull(mediaItem, "mediaItem");
+            Assert.ArgumentNotNull(path, "path");
+            if (args.Destination == UploadDestination.Database)
+            {
+                Log.Info("Media Item has been uploaded to database: " + path, this);
+            }
+            else
+            {
+                Log.Info("Media Item has been uploaded to file system: " + path, this);
+            }
+            args.UploadedItems.Add(mediaItem.InnerItem);
+        }
+
+        /// <summary>
+        /// Unpacks to file.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        /// <param name="file">The file.</param>
+        private static void UnpackToFile(UploadArgs args, HttpPostedFile file)
+        {
+            Assert.ArgumentNotNull(args, "args");
+            Assert.ArgumentNotNull(file, "file");
+            string filename = FileUtil.MapPath(TempFolder.GetFilename("temp.zip"));
+            file.SaveAs(filename);
+            using (ZipReader zipReader = new ZipReader(filename))
+            {
+                using (IEnumerator<ZipEntry> enumerator = zipReader.Entries.GetEnumerator())
+                {
+                    while (enumerator.MoveNext())
+                    {
+                        ZipEntry entry = enumerator.Current;
+                        if (Path.GetInvalidFileNameChars().Any((char ch) => entry.Name.Contains(ch)))
+                        {
+                            string text = string.Format("The \"{0}\" file was not uploaded because it contains malicious file: \"{1}\"", file.FileName, entry.Name);
+                            Log.Warn(text, typeof(Save));
+                            args.UiResponseHandlerEx.MaliciousFile(StringUtil.EscapeJavascriptString(file.FileName));
+                            args.ErrorText = text;
+                            args.AbortPipeline();
+                            return;
+                        }
+                    }
+                }
+                foreach (ZipEntry current in zipReader.Entries)
+                {
+                    string text2 = FileUtil.MakePath(args.Folder, current.Name, '\\');
+                    if (current.IsDirectory)
+                    {
+                        Directory.CreateDirectory(text2);
+                    }
+                    else
+                    {
+                        if (!args.Overwrite)
+                        {
+                            text2 = FileUtil.GetUniqueFilename(text2);
+                        }
+                        Directory.CreateDirectory(Path.GetDirectoryName(text2));
+                        object fileLock = FileUtil.GetFileLock(text2);
+                        lock (fileLock)
+                        {
+                            FileUtil.CreateFile(text2, current.GetStream(), true);
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Uploads to file.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        /// <param name="file">The file.</param>
+        /// <returns>The name of the uploaded file</returns>
+        private string UploadToFile(UploadArgs args, HttpPostedFile file)
+        {
+            Assert.ArgumentNotNull(args, "args");
+            Assert.ArgumentNotNull(file, "file");
+            string text = FileUtil.MakePath(args.Folder, Path.GetFileName(file.FileName), '\\');
+            if (!args.Overwrite)
+            {
+                text = FileUtil.GetUniqueFilename(text);
+            }
+            file.SaveAs(text);
+            Log.Info("File has been uploaded: " + text, this);
+            return Assert.ResultNotNull<string>(text);
+        }
+    }
+}
