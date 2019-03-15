a8f464331de65506e8418bae171de8594f7a45c3
Zadli <zadlizah@gmail.com>
2019-03-15 09:35:08 +0800

    Commit Second Time

--- src/Sitecore.Support.90405/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Sitecore.Support.90405")]
-[assembly: AssemblyProduct("Sitecore.Support.90405")]
-[assembly: ComVisible(false)]
